4cba58a120a35537c9c11d65888f0614296cf99b
ydkim <yedami93@gmail.com>
2024-02-06 17:10:36 +0900

    비디오 페이지 진입 버튼 추가 (임시)

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -4,15 +4,30 @@
       Co-Meet !
     </h1>
     <div class="app-home__buttons buttons__center">
-      <el-button>회원가입</el-button>
-      <el-button type="primary">
+      <el-button disabled>
+        회원가입
+      </el-button>
+      <el-button
+        type="primary"
+        disabled>
         로그인
       </el-button>
+      <div class="buttons__center">
+        <el-button
+          class="test-btn"
+          type="primary"
+          size="large"
+          @click="router.push({ name: 'video-chats' })">
+          새 회의
+        </el-button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
 
 </script>
 
@@ -31,6 +46,11 @@
     position: absolute;
     top: 40%;
     left: 80px;
+    width: 200px;
   }
+}
+.test-btn { 
+  width: 100%;
+  margin-top: $gap-m;
 }
 </style>
